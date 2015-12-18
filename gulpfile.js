12fab5938fb24be2d5ea29efb6bf9d08d0a21312
Łukasz Węgiel <lukasz@mirumee.com>
2015-12-18 14:40:49 +0100

    Add watch gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,30 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 
-gulp.task('default', function () {
+gulp.task('js', function () {
     return browserify('./source/app.js')
         .transform(babelify, { presets: ["react"] } )
         .bundle()
         .pipe(source('snapterest.js'))
         .pipe(gulp.dest('./build/'));
 });
+
+gulp.task('js-watch', ['js'], function() {
+    browserSync.reload();
+});
+
+gulp.task('browser-sync', function () {
+
+    browserSync.init({
+        server: {
+            baseDir: "./build/"
+        }
+    });
+});
+
+gulp.task('default', ['browser-sync'], function() {
+    gulp.watch('./source/**/*.js', ['js-watch']);
+})
