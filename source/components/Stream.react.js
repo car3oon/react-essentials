17a9f6824461852228a78a8374ecf4fd3750d63a
Łukasz Węgiel <lukasz@mirumee.com>
2015-12-22 13:31:39 +0100

    Chapter 6

--- source/components/Stream.react.js
+++ source/components/Stream.react.js
@@ -4,41 +4,42 @@ var StreamTweet = require('./StreamTweet.react');
 var Header = require('./Header.react');
 
 var Stream = React.createClass({
+  
+  getInitialState: function () {
+    return {
+      tweet: null 
+    };
+  },
 
-    getInitialState: function() {
-        return {
-            tweet: null
-        };
-    },
+  componentDidMount: function () {
+    SnapkiteStreamClient.initializeStream(this.handleNewTweet);
+  },
 
-    componentDidMount: function() {
-        SnapkiteStreamClient.initializeStream(this.handleNewTweet);
-    },
+  componentWillUnmount: function () {
+    SnapkiteStreamClient.destroyStream();
+  },
 
-    componentWillUnmount: function() {
-        SnapkiteStreamClient.destroyStream();
-    },
+  handleNewTweet: function (tweet) {
+    this.setState({
+      tweet: tweet
+    });
+  },
 
-    handleNewTweet: function() {
-        setState({
-            tweet: tweet
-        });
-    },
+  render: function () {
+    var tweet = this.state.tweet;
 
-    render: function() {
-        var tweet = this.state.tweet;
-
-        if (tweet) {
-            return (
-                <StreamTweet tweet={tweet}
-                    onAddTweetToCollection={this.props.onAddTweetToCollection} />
-            );
-        }
-        return (
-            <Header text="Waiting for public photos from Twitter..." />
-        );
+    if (tweet) {
+      return (
+        <StreamTweet
+          tweet={tweet}
+          onAddTweetToCollection={this.props.onAddTweetToCollection} />
+      );
     }
 
+    return (
+      <Header text="Waiting for public photos from Twitter..." />
+    ); 
+  }
 });
 
 module.exports = Stream;
