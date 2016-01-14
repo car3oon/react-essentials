27fe4487db3a79eb5745d300930ba9ed4d96f8a5
Łukasz Węgiel <lukasz@mirumee.com>
2016-01-14 12:36:01 +0100

    Chapter 9 - final

---
+++ source/stores/TweetStore.js
@@ -0,0 +1,40 @@
+var Dispatcher = require('../dispatcher/AppDispatcher');
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
+
+var tweet = null;
+
+function setTweet(recivedTweet) {
+    tweet = recivedTweet;
+}
+
+function emitChanges() {
+    TweetStore.emit('change');
+}
+
+var TweetStore = assign({}, EventEmitter.prototype, {
+    addChangeListener: function(callback) {
+        this.on('change', callback);
+    },
+
+    removeChangeListener: function(callback) {
+        this.removeListener('change', callback);
+    },
+
+    getTweet: function () {
+        return tweet;
+    }
+
+});
+
+
+function handleAction(action) {
+    if (action.type === 'recive_tweet') {
+        setTweet(action.tweet);
+        emitChange();
+    }
+}
+
+TweetStore.dispatchToken = AppDispatcher.register(handleAction);
+
+module.exports = TweetStore;
